b7ffb27e1c0283cb041e3113c430f0868d4b7404
PANOV <novpavel06@gmail.com>
2024-03-17 15:44:23 +0300

    Update MainActivity.kt

--- app/src/main/java/com/panov/timetable/MainActivity.kt
+++ app/src/main/java/com/panov/timetable/MainActivity.kt
@@ -4,36 +4,34 @@ import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.forEach
-import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private val clockFragment = ClockFragment()
-    private val timetableFragment = TimetableFragment()
-    private val settingsFragment = SettingsFragment()
+    companion object {
+        private var selectedItem = R.id.menu_timetable
+        private val clockFragment = ClockFragment()
+        private val timetableFragment = TimetableFragment()
+        private val settingsFragment = SettingsFragment()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val mainMenu = findViewById<BottomNavigationView>(R.id.mainMenu)
-        mainMenu.menu.forEach {
-            mainMenu.findViewById<View>(it.itemId).setOnLongClickListener { true }
-        }
-        mainMenu.selectedItemId = R.id.menu_timetable
-        openFragment(timetableFragment)
-
-        mainMenu.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.menu_clock -> openFragment(clockFragment)
-                R.id.menu_timetable -> openFragment(timetableFragment)
-                R.id.menu_settings -> openFragment(settingsFragment)
-            }
-            true
-        }
+        mainMenu.menu.forEach { mainMenu.findViewById<View>(it.itemId).setOnLongClickListener { true } }
+        mainMenu.setOnItemSelectedListener { selectItem(it.itemId) }
+        mainMenu.findViewById<View>(selectedItem).performClick()
     }
 
-    private fun openFragment(fragment: Fragment) {
+    private fun selectItem(item: Int): Boolean {
+        selectedItem = item
+        val fragment = when (item) {
+            R.id.menu_clock -> clockFragment
+            R.id.menu_settings -> settingsFragment
+            else -> timetableFragment
+        }
         supportFragmentManager.beginTransaction().replace(R.id.mainView, fragment).commit()
+        return true
     }
 }
