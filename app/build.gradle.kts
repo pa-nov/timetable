c87661b4db67d86bfa1cf2ac4926f3a25a7bd549
PANOV <novpavel06@gmail.com>
2024-03-17 03:43:38 +0300

    Added main activity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
 }
 
 android {
@@ -12,7 +12,7 @@ android {
         minSdk = 31
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0.0"
+        versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -33,18 +33,16 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
-    buildFeatures {
-        viewBinding = true
-    }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
 }
--- app/src/main/java/com/panov/timetable/MainActivity.kt
+++ app/src/main/java/com/panov/timetable/MainActivity.kt
@@ -1,30 +1,39 @@
 package com.panov.timetable
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.forEach
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private val clockFragment = ClockFragment()
+    private val timetableFragment = TimetableFragment()
+    private val settingsFragment = SettingsFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<BottomNavigationView>(R.id.menu).selectedItemId = R.id.timetable
-        openFragment(TimetableFragment())
+        val mainMenu = findViewById<BottomNavigationView>(R.id.mainMenu)
+        mainMenu.menu.forEach {
+            mainMenu.findViewById<View>(it.itemId).setOnLongClickListener { true }
+        }
+        mainMenu.selectedItemId = R.id.menu_timetable
+        openFragment(timetableFragment)
 
-        findViewById<BottomNavigationView>(R.id.menu).setOnItemSelectedListener {
-            when(it.itemId) {
-                R.id.clock     -> openFragment(ClockFragment())
-                R.id.timetable -> openFragment(TimetableFragment())
-                R.id.settings  -> openFragment(SettingsFragment())
-                else -> { }
+        mainMenu.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.menu_clock -> openFragment(clockFragment)
+                R.id.menu_timetable -> openFragment(timetableFragment)
+                R.id.menu_settings -> openFragment(settingsFragment)
             }
             true
         }
     }
 
     private fun openFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+        supportFragmentManager.beginTransaction().replace(R.id.mainView, fragment).commit()
     }
 }
