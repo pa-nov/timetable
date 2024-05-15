a1b3b4b978025f699b947940d9b9f094990321cd
PANOV <novpavel06@gmail.com>
2024-05-15 21:05:07 +0300

    Refactoring

---
+++ app/src/main/java/com/panov/timetable/TimetableData.kt
@@ -0,0 +1,10 @@
+package com.panov.timetable
+
+class TimetableData(currentI: Int, currentN: Int, currentD: Int, nextI: Int, nextN: Int, nextD: Int) {
+    val currentId = currentI
+    val currentNumber = currentN
+    val currentDays = currentD
+    val nextId = nextI
+    val nextNumber = nextN
+    val nextDays = nextD
+}
