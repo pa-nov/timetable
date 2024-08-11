f77c977f6b851231943cdddb72691d2696f90969
PANOV <novpavel06@gmail.com>
2024-08-11 15:05:39 +0300

    Added: lesson info popup

--- app/src/main/java/com/panov/timetable/TimetableAdapter.kt
+++ app/src/main/java/com/panov/timetable/TimetableAdapter.kt
@@ -1,11 +1,16 @@
 package com.panov.timetable
 
+import android.content.Context
+import android.graphics.Outline
 import android.icu.util.Calendar
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.MeasureSpec
 import android.view.ViewGroup
+import android.view.ViewOutlineProvider
 import android.widget.FrameLayout
 import android.widget.LinearLayout
+import android.widget.PopupWindow
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.panov.timetable.fragment.TimetableFragment
@@ -51,6 +56,11 @@ class TimetableAdapter(private val fragment: TimetableFragment) : RecyclerView.A
                 val lessonIndex = index - 1
 
                 fillLessonView(item, timetable, lessonIndex, day, week)
+                if (timetable.getLessonId(week, day, lessonIndex) > 0) {
+                    item.setOnClickListener { showInfoPopup(item, timetable, lessonIndex, day, week) }
+                } else {
+                    item.setOnClickListener(null)
+                }
             }
         } else {
             view.findViewById<TextView>(R.id.title_weekday).text = view.context.getString(R.string.message_error)
@@ -90,6 +100,110 @@ class TimetableAdapter(private val fragment: TimetableFragment) : RecyclerView.A
             textTeacher.text = ""
             textClassroom.text = ""
         }
+    }
+
+    private fun showInfoPopup(item: View, timetable: TimetableData, lessonIndex: Int, day: Int, week: String, context: Context = item.context) {
+        val view = LayoutInflater.from(context).inflate(R.layout.popup_lesson_info, null, false)
+        view.clipToOutline = true
+        view.outlineProvider = object : ViewOutlineProvider() {
+            val radius = Converter.getPxFromDp(context, 16).toFloat()
+            override fun getOutline(view: View, outline: Outline) {
+                outline.setRoundRect(0, 0, view.width, view.height, radius)
+            }
+        }
+
+        val lessonIds = arrayListOf(timetable.getLessonId(week, day, lessonIndex))
+        lessonIds.addAll(timetable.getLessonOtherIds(lessonIds[0]))
+        lessonIds.sort()
+
+
+        val titleTitle = view.findViewById<TextView>(R.id.title_title)
+        val titleTeacher = view.findViewById<TextView>(R.id.title_teacher)
+        val titleClassroom = view.findViewById<TextView>(R.id.title_classroom)
+        val textTitle = view.findViewById<TextView>(R.id.text_title)
+        val textTeacher = view.findViewById<TextView>(R.id.text_teacher)
+        val textClassroom = view.findViewById<TextView>(R.id.text_classroom)
+
+        val titles = arrayListOf<String>()
+        val teachers = arrayListOf<String>()
+        val classrooms = arrayListOf<String>()
+        for (lessonId in lessonIds) {
+            val title = timetable.getLessonFullTitle(lessonId)
+            val teacher = timetable.getTeacherFullName(lessonId)
+            val classroom = timetable.getLessonClassroom(lessonId)
+
+            if (title !in titles) titles.add(title)
+            if (teacher !in teachers) teachers.add(teacher)
+            if (classroom !in classrooms) classrooms.add(classroom)
+        }
+
+        if (titles.count() > 1) titleTitle.text = context.getString(R.string.info_titles)
+        if (teachers.count() > 1) titleTeacher.text = context.getString(R.string.info_teachers)
+        if (classrooms.count() > 1) titleClassroom.text = context.getString(R.string.info_classrooms)
+        textTitle.text = titles.joinToString(",\n")
+        textTeacher.text = teachers.joinToString(",\n")
+        textClassroom.text = classrooms.joinToString(",\n")
+
+
+        val layoutEven = view.findViewById<LinearLayout>(R.id.layout_even)
+        val layoutOdd = view.findViewById<LinearLayout>(R.id.layout_odd)
+        val titleEven = view.findViewById<TextView>(R.id.title_even)
+        val titleOdd = view.findViewById<TextView>(R.id.title_odd)
+
+        var isDifferent = false
+        for (tempDay in 0 until 7) {
+            var tempLayoutEven: LinearLayout? = null
+            var tempLayoutOdd: LinearLayout? = null
+
+            for (tempIndex in 0 until timetable.getLessonsCount()) {
+                val lessonEvenId = timetable.getLessonId("even", tempDay, tempIndex)
+                val lessonOddId = timetable.getLessonId("odd", tempDay, tempIndex)
+
+                if (lessonEvenId in lessonIds) {
+                    if (tempLayoutEven == null) tempLayoutEven = createDayLayout(context, tempDay)
+                    val tempItem = LayoutInflater.from(context).inflate(R.layout.item_lesson, null, false) as LinearLayout
+                    val isNow = tempIndex == lessonIndex && tempDay == day && "even" == week
+                    fillLessonView(tempItem, timetable, tempIndex, tempDay, "even", isNow)
+                    tempLayoutEven.addView(tempItem)
+                }
+
+                if (lessonOddId in lessonIds) {
+                    if (tempLayoutOdd == null) tempLayoutOdd = createDayLayout(context, tempDay)
+                    val tempItem = LayoutInflater.from(context).inflate(R.layout.item_lesson, null, false) as LinearLayout
+                    val isNow = tempIndex == lessonIndex && tempDay == day && "odd" == week
+                    fillLessonView(tempItem, timetable, tempIndex, tempDay, "odd", isNow)
+                    tempLayoutOdd.addView(tempItem)
+                }
+
+                if (lessonEvenId != lessonOddId && (lessonEvenId in lessonIds || lessonOddId in lessonIds)) isDifferent = true
+            }
+
+            if (tempLayoutEven != null) layoutEven.addView(tempLayoutEven)
+            if (tempLayoutOdd != null) layoutOdd.addView(tempLayoutOdd)
+        }
+
+        if (!isDifferent) {
+            (if (week == "even") layoutOdd else layoutEven).visibility = View.GONE
+            (if (week == "even") titleEven else titleOdd).text = context.getString(R.string.week_every)
+        }
+
+
+        val fragmentHeight = fragment.requireView().height
+        val itemLocationArray = IntArray(2)
+        item.getLocationOnScreen(itemLocationArray)
+        view.measure(MeasureSpec.makeMeasureSpec(item.width, MeasureSpec.EXACTLY), MeasureSpec.makeMeasureSpec(fragmentHeight / 2, MeasureSpec.AT_MOST))
+        val direction = if (itemLocationArray[1] + view.measuredHeight > fragmentHeight + Converter.getPxFromDp(context, 64)) 1 else -1
+        PopupWindow(view, view.measuredWidth, view.measuredHeight, true).showAsDropDown(item, 0, item.height * direction)
+    }
+
+    private fun createDayLayout(context: Context, day: Int): LinearLayout {
+        val layout = LayoutInflater.from(context).inflate(R.layout.page_timetable, null, false) as LinearLayout
+
+        val title = layout.findViewById<TextView>(R.id.title_weekday)
+        title.text = context.resources.getStringArray(R.array.weekdays)[day]
+        title.textSize = 20f
+
+        return layout
     }
 }
 
