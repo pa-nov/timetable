7402df37e16162a743450393d5d2f0de0a2eb6cd
PANOV <novpavel06@gmail.com>
2024-08-21 18:21:51 +0300

    Updated: TimetableData

--- app/src/main/kotlin/com/panov/util/TimetableData.kt
+++ app/src/main/kotlin/com/panov/util/TimetableData.kt
@@ -5,8 +5,8 @@ import org.json.JSONArray
 import org.json.JSONObject
 
 class TimetableData(jsonString: String) {
-    private val times: Array<TimetableTime>
-    private val lessons: Array<TimetableLesson>
+    private val times: Array<Time>
+    private val lessons: Array<Lesson>
     private val even: Array<Array<Int>>
     private val odd: Array<Array<Int>>
 
@@ -17,15 +17,15 @@ class TimetableData(jsonString: String) {
         val jsonEven = jsonObject.getJSONArray("even")
         val jsonOdd = jsonObject.getJSONArray("odd")
 
-        val timesList = arrayListOf<TimetableTime>()
+        val timesList = arrayListOf<Time>()
         for (index in 0 until jsonTimes.length()) {
-            timesList.add(TimetableTime(jsonTimes.getJSONArray(index)))
+            timesList.add(Time(jsonTimes.getJSONArray(index)))
         }
         times = timesList.toTypedArray()
 
-        val lessonsList = arrayListOf<TimetableLesson>()
+        val lessonsList = arrayListOf<Lesson>()
         for (index in 0 until jsonLessons.length()) {
-            lessonsList.add(TimetableLesson(jsonLessons.getJSONArray(index)))
+            lessonsList.add(Lesson(jsonLessons.getJSONArray(index)))
         }
         lessons = lessonsList.toTypedArray()
 
@@ -99,195 +99,202 @@ class TimetableData(jsonString: String) {
         if (week == "odd") return odd[day][lessonIndex]
         return 0
     }
-}
 
-class TimetableTime(jsonArray: JSONArray) {
-    private val startHour: Int = jsonArray.getString(0).toIntOrNull() ?: 0
-    private val startMinute: Int = jsonArray.getString(1).toIntOrNull() ?: 0
-    private val endHour: Int = jsonArray.getString(2).toIntOrNull() ?: 0
-    private val endMinute: Int = jsonArray.getString(3).toIntOrNull() ?: 0
-
-    fun getStartSeconds(): Int {
-        return ((startHour * 60) + startMinute) * 60
+    fun getOffset(calendar: Calendar): Offset {
+        return Offset(this, calendar)
     }
 
-    fun getEndSeconds(): Int {
-        return ((endHour * 60) + endMinute) * 60
-    }
 
-    override fun toString(): String {
-        val start = "${Converter.getTwoDigitNumber(startHour)}:${Converter.getTwoDigitNumber(startMinute)}"
-        val end = "${Converter.getTwoDigitNumber(endHour)}:${Converter.getTwoDigitNumber(endMinute)}"
-        return "$start $end"
-    }
-}
+    class Time(jsonArray: JSONArray) {
+        private val startHour: Int = jsonArray.getString(0).toIntOrNull() ?: 0
+        private val startMinute: Int = jsonArray.getString(1).toIntOrNull() ?: 0
+        private val endHour: Int = jsonArray.getString(2).toIntOrNull() ?: 0
+        private val endMinute: Int = jsonArray.getString(3).toIntOrNull() ?: 0
 
-class TimetableLesson(jsonArray: JSONArray) {
-    private val titleFull: String
-    private val titleShort: String
-    private val classroom: String = jsonArray.getString(1).trim()
-    private val teacherLastName: String
-    private val teacherFirstName: String
-    private val teacherMiddleName: String
-    private val other: Array<Int>
-
-    init {
-        val titles = jsonArray.getString(0).split("|")
-        titleFull = titles[0].trim()
-        titleShort = if (titles.count() > 1) titles[1].trim() else titleFull
-
-        val teacherNames = jsonArray.getString(2).split("|")
-        teacherLastName = teacherNames[0].trim()
-        teacherFirstName = if (teacherNames.count() > 1) teacherNames[1].trim() else ""
-        teacherMiddleName = if (teacherNames.count() > 2) teacherNames[2].trim() else ""
-
-        val others = jsonArray.getString(3).split("|")
-        val otherList = arrayListOf<Int>()
-        for (index in 0 until others.count()) {
-            val number = others[index].toIntOrNull() ?: 0
-            if (number > 0) otherList.add(number)
+        fun getStartSeconds(): Int {
+            return ((startHour * 60) + startMinute) * 60
         }
-        other = otherList.toTypedArray()
+
+        fun getEndSeconds(): Int {
+            return ((endHour * 60) + endMinute) * 60
+        }
+
+        override fun toString(): String {
+            val start = "${Converter.getTwoDigitNumber(startHour)}:${Converter.getTwoDigitNumber(startMinute)}"
+            val end = "${Converter.getTwoDigitNumber(endHour)}:${Converter.getTwoDigitNumber(endMinute)}"
+            return "$start $end"
+        }
     }
 
-    fun getFullTitle(): String {
-        return titleFull
-    }
 
-    fun getShortTitle(): String {
-        return titleShort
-    }
+    class Lesson(jsonArray: JSONArray) {
+        private val titleFull: String
+        private val titleShort: String
+        private val classroom: String = jsonArray.getString(1).trim()
+        private val teacherLastName: String
+        private val teacherFirstName: String
+        private val teacherMiddleName: String
+        private val other: Array<Int>
 
-    fun getClassroom(): String {
-        return classroom
-    }
+        init {
+            val titles = jsonArray.getString(0).split("|")
+            titleFull = titles[0].trim()
+            titleShort = if (titles.count() > 1) titles[1].trim() else titleFull
 
-    fun getClassroomText(): String {
-        return "($classroom)"
-    }
+            val teacherNames = jsonArray.getString(2).split("|")
+            teacherLastName = teacherNames[0].trim()
+            teacherFirstName = if (teacherNames.count() > 1) teacherNames[1].trim() else ""
+            teacherMiddleName = if (teacherNames.count() > 2) teacherNames[2].trim() else ""
 
-    fun getTeacherFullName(): String {
-        var teacherName = "$teacherLastName "
-        if (teacherFirstName.isNotEmpty()) {
-            teacherName += "$teacherFirstName "
-            if (teacherMiddleName.isNotEmpty()) {
-                teacherName += teacherMiddleName
+            val others = jsonArray.getString(3).split("|")
+            val otherList = arrayListOf<Int>()
+            for (index in 0 until others.count()) {
+                val number = others[index].toIntOrNull() ?: 0
+                if (number > 0) otherList.add(number)
             }
+            other = otherList.toTypedArray()
         }
-        return teacherName.trim().ifEmpty { "-" }
-    }
 
-    fun getTeacherShortName(): String {
-        var teacherName = ""
-        if (teacherLastName.isNotEmpty()) {
-            teacherName += teacherLastName
+        fun getFullTitle(): String {
+            return titleFull
+        }
+
+        fun getShortTitle(): String {
+            return titleShort
+        }
+
+        fun getClassroom(): String {
+            return classroom
+        }
+
+        fun getClassroomText(): String {
+            return "($classroom)"
+        }
+
+        fun getTeacherFullName(): String {
+            var teacherName = "$teacherLastName "
             if (teacherFirstName.isNotEmpty()) {
-                teacherName += " ${teacherFirstName[0]}."
+                teacherName += "$teacherFirstName "
                 if (teacherMiddleName.isNotEmpty()) {
-                    teacherName += " ${teacherMiddleName[0]}."
+                    teacherName += teacherMiddleName
                 }
             }
+            return teacherName.trim().ifEmpty { "-" }
         }
-        return teacherName.ifEmpty { "-" }
+
+        fun getTeacherShortName(): String {
+            var teacherName = ""
+            if (teacherLastName.isNotEmpty()) {
+                teacherName += teacherLastName
+                if (teacherFirstName.isNotEmpty()) {
+                    teacherName += " ${teacherFirstName[0]}."
+                    if (teacherMiddleName.isNotEmpty()) {
+                        teacherName += " ${teacherMiddleName[0]}."
+                    }
+                }
+            }
+            return teacherName.ifEmpty { "-" }
+        }
+
+        fun getOtherIds(): Array<Int> {
+            return other
+        }
     }
 
-    fun getOtherIds(): Array<Int> {
-        return other
-    }
-}
 
-class TimetableOffset(timetable: TimetableData, calendar: Calendar) {
-    val currentLessonIndex: Int
-    val currentDay: Int
-    val currentWeek: String
-    val currentDaysOffset: Int
+    class Offset(timetable: TimetableData, calendar: Calendar) {
+        val currentLessonIndex: Int
+        val currentDay: Int
+        val currentWeek: String
+        val currentDaysOffset: Int
 
-    val nextLessonIndex: Int
-    val nextDay: Int
-    val nextWeek: String
-    val nextDaysOffset: Int
+        val nextLessonIndex: Int
+        val nextDay: Int
+        val nextWeek: String
+        val nextDaysOffset: Int
 
-    init {
-        val day = if (calendar.get(Calendar.DAY_OF_WEEK) > 1) calendar.get(Calendar.DAY_OF_WEEK) - 2 else 6
-        val week = if (calendar.get(Calendar.WEEK_OF_YEAR) % 2 == 0) "even" else "odd"
-        val seconds = calendar.get(Calendar.MILLISECONDS_IN_DAY) / 1000
-        val lessonsCount = timetable.getLessonsCount()
+        init {
+            val day = if (calendar.get(Calendar.DAY_OF_WEEK) > 1) calendar.get(Calendar.DAY_OF_WEEK) - 2 else 6
+            val week = if (calendar.get(Calendar.WEEK_OF_YEAR) % 2 == 0) "even" else "odd"
+            val seconds = calendar.get(Calendar.MILLISECONDS_IN_DAY) / 1000
+            val lessonsCount = timetable.getLessonsCount()
 
-        var tempLessonIndex = -1
-        var tempDay = day
-        var tempWeek = week
-        var tempDaysOffset = 0
+            var tempLessonIndex = -1
+            var tempDay = day
+            var tempWeek = week
+            var tempDaysOffset = 0
 
-        for (index in 0 until lessonsCount) {
-            if (timetable.getLessonTimeStart(index) > seconds) break
-            tempLessonIndex = index
-        }
-        var tempOffset = lessonsCount * 14
-        while (tempOffset >= 0) {
-            if (tempLessonIndex < 0) {
-                tempLessonIndex = lessonsCount - 1
-                tempDaysOffset--
-                tempDay--
-                if (tempDay < 0) {
-                    tempDay = 6
-                    tempWeek = if (tempWeek == "odd") "even" else "odd"
-                }
+            for (index in 0 until lessonsCount) {
+                if (timetable.getLessonTimeStart(index) > seconds) break
+                tempLessonIndex = index
             }
-            if (timetable.getLessonId(tempWeek, tempDay, tempLessonIndex) > 0) {
-                tempOffset = 0
-            } else {
-                if (tempOffset > 0) {
-                    tempLessonIndex--
+            var tempOffset = lessonsCount * 14
+            while (tempOffset >= 0) {
+                if (tempLessonIndex < 0) {
+                    tempLessonIndex = lessonsCount - 1
+                    tempDaysOffset--
+                    tempDay--
+                    if (tempDay < 0) {
+                        tempDay = 6
+                        tempWeek = if (tempWeek == "odd") "even" else "odd"
+                    }
                 }
-            }
-            tempOffset--
-        }
-
-        currentLessonIndex = tempLessonIndex
-        currentDay = tempDay
-        currentWeek = tempWeek
-        currentDaysOffset = tempDaysOffset
-    }
-
-    init {
-        val day = if (calendar.get(Calendar.DAY_OF_WEEK) > 1) calendar.get(Calendar.DAY_OF_WEEK) - 2 else 6
-        val week = if (calendar.get(Calendar.WEEK_OF_YEAR) % 2 == 0) "even" else "odd"
-        val seconds = calendar.get(Calendar.MILLISECONDS_IN_DAY) / 1000
-        val lessonsCount = timetable.getLessonsCount()
-
-        var tempLessonIndex = 0
-        var tempDay = day
-        var tempWeek = week
-        var tempDaysOffset = 0
-
-        for (index in 0 until lessonsCount) {
-            if (timetable.getLessonTimeStart(index) > seconds) break
-            tempLessonIndex = index + 1
-        }
-        var tempOffset = lessonsCount * 14
-        while (tempOffset >= 0) {
-            if (tempLessonIndex >= lessonsCount) {
-                tempLessonIndex = 0
-                tempDaysOffset++
-                tempDay++
-                if (tempDay > 6) {
-                    tempDay = 0
-                    tempWeek = if (tempWeek == "odd") "even" else "odd"
+                if (timetable.getLessonId(tempWeek, tempDay, tempLessonIndex) > 0) {
+                    tempOffset = 0
+                } else {
+                    if (tempOffset > 0) {
+                        tempLessonIndex--
+                    }
                 }
+                tempOffset--
             }
-            if (timetable.getLessonId(tempWeek, tempDay, tempLessonIndex) > 0) {
-                tempOffset = 0
-            } else {
-                if (tempOffset > 0) {
-                    tempLessonIndex++
-                }
-            }
-            tempOffset--
+
+            currentLessonIndex = tempLessonIndex
+            currentDay = tempDay
+            currentWeek = tempWeek
+            currentDaysOffset = tempDaysOffset
         }
 
-        nextLessonIndex = tempLessonIndex
-        nextDay = tempDay
-        nextWeek = tempWeek
-        nextDaysOffset = tempDaysOffset
+        init {
+            val day = if (calendar.get(Calendar.DAY_OF_WEEK) > 1) calendar.get(Calendar.DAY_OF_WEEK) - 2 else 6
+            val week = if (calendar.get(Calendar.WEEK_OF_YEAR) % 2 == 0) "even" else "odd"
+            val seconds = calendar.get(Calendar.MILLISECONDS_IN_DAY) / 1000
+            val lessonsCount = timetable.getLessonsCount()
+
+            var tempLessonIndex = 0
+            var tempDay = day
+            var tempWeek = week
+            var tempDaysOffset = 0
+
+            for (index in 0 until lessonsCount) {
+                if (timetable.getLessonTimeStart(index) > seconds) break
+                tempLessonIndex = index + 1
+            }
+            var tempOffset = lessonsCount * 14
+            while (tempOffset >= 0) {
+                if (tempLessonIndex >= lessonsCount) {
+                    tempLessonIndex = 0
+                    tempDaysOffset++
+                    tempDay++
+                    if (tempDay > 6) {
+                        tempDay = 0
+                        tempWeek = if (tempWeek == "odd") "even" else "odd"
+                    }
+                }
+                if (timetable.getLessonId(tempWeek, tempDay, tempLessonIndex) > 0) {
+                    tempOffset = 0
+                } else {
+                    if (tempOffset > 0) {
+                        tempLessonIndex++
+                    }
+                }
+                tempOffset--
+            }
+
+            nextLessonIndex = tempLessonIndex
+            nextDay = tempDay
+            nextWeek = tempWeek
+            nextDaysOffset = tempDaysOffset
+        }
     }
 }
