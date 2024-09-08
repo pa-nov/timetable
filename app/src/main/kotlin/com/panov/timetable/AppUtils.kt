be7531717477e3f0124b1ec84dd7d0ab71c10f87
PANOV <novpavel06@gmail.com>
2024-09-08 23:15:32 +0300

    Added: lesson widget

--- app/src/main/kotlin/com/panov/timetable/AppUtils.kt
+++ app/src/main/kotlin/com/panov/timetable/AppUtils.kt
@@ -18,4 +18,20 @@ object AppUtils {
 
         return context.createConfigurationContext(config)
     }
+
+    fun getModifiedCalendar(settings: SettingsData): Calendar {
+        val calendar = Calendar.getInstance()
+        calendar.firstDayOfWeek = Calendar.MONDAY
+        calendar.minimalDaysInFirstWeek = 4
+
+        val modifierHour = settings.getInt(Storage.Widgets.MODIFIER_HOUR, 1)
+        val modifierMinute = settings.getInt(Storage.Widgets.MODIFIER_MINUTE, 1)
+        val modifierSecond = settings.getInt(Storage.Widgets.MODIFIER_SECOND, 1)
+
+        calendar.set(Calendar.HOUR_OF_DAY, if (modifierHour > 0) calendar.get(Calendar.HOUR_OF_DAY) / modifierHour * modifierHour else -modifierHour)
+        calendar.set(Calendar.MINUTE, if (modifierMinute > 0) calendar.get(Calendar.MINUTE) / modifierMinute * modifierMinute else -modifierMinute)
+        calendar.set(Calendar.SECOND, if (modifierSecond > 0) calendar.get(Calendar.SECOND) / modifierSecond * modifierSecond else -modifierSecond)
+
+        return calendar
+    }
 }
