4124cdacba502ef1bd9c3e565f8327a4e048850b
pa-nov <novpavel06@gmail.com>
2026-01-06 23:24:20 +0300

    Removed: ConstraintLayout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,5 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     implementation(libs.android.material)
 }
